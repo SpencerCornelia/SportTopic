96c8ad0d7dc7ddd73cefb85e3f9bfa3e2882c413
Spencer Cornelia <cornelia.spencer@yahoo.com>
2015-07-15 09:57:34 -0700

    team roster with d3

--- index.js
+++ index.js
@@ -107,6 +107,11 @@ app.get("/d3WSPointGuard", function (req, res) {
 	res.sendFile(thisPath);
 });
 
+app.get("/d3RosterBetterTeam", function (req, res) {
+	var thisPath = path.join(views, "d3RosterBetterTeam.html");
+	res.sendFile(thisPath);
+});
+
 io.sockets.on("connection", function(socket) {
 	socket.on("send message", function(data) {
 		io.sockets.emit("new message", data);
